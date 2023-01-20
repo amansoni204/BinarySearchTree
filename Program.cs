f43d90e27a275c54fe7b9b54fe98ed097e9f86db
amansoni <amansoni2041999@gmail.com>
2023-01-20 15:10:18 +0530

    performing BinarysearchTree program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Binary SearchTree");
+            BinarySearch<int> binarySearchTree = new BinarySearch<int>(56);
+            binarySearchTree.AddNode(30);
+            binarySearchTree.AddNode(70);
+            binarySearchTree.Display();
+
+        }
+    }
+}
