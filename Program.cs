373d408e9f4362342c52a4d980053e7e107aa7d4
amansoni <amansoni2041999@gmail.com>
2023-01-20 15:19:29 +0530

    performing Binarysearchtree program

--- Program.cs
+++ Program.cs
@@ -14,6 +14,16 @@ namespace BinarySearchTree
             BinarySearch<int> binarySearchTree = new BinarySearch<int>(56);
             binarySearchTree.AddNode(30);
             binarySearchTree.AddNode(70);
+            binarySearchTree.AddNode(22);
+            binarySearchTree.AddNode(44);
+            binarySearchTree.AddNode(60);
+            binarySearchTree.AddNode(95);
+            binarySearchTree.AddNode(11);
+            binarySearchTree.AddNode(65);
+            binarySearchTree.AddNode(3);
+            binarySearchTree.AddNode(16);
+            binarySearchTree.AddNode(63);
+            binarySearchTree.AddNode(67);
             binarySearchTree.Display();
 
         }
